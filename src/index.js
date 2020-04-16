434b10fa6e03bb62fe8ab1a6f3bcdeb078ea1bc2
Ashley Kolodziej <kolodz10@bu.edu>
2020-04-16 15:42:46 -0400

    Give Painterro a shot

--- src/index.js
+++ src/index.js
@@ -1,11 +1,42 @@
 'use strict';
 
 import "./styles.css";
+import Painterro from 'painterro';
+
+Painterro({
+	activeColor: '#00ff00', // default brush color is green
+	toolbarPosition: 'top',
+	hiddenTools: [
+		'select',
+		'settings',
+		'undo',
+		'redo',
+		'pixelize',
+		'crop',
+		//'eraser',
+		//'line',
+		'arrow',
+		'rect',
+		'ellipse',
+		'brush',
+		'text',
+		'rotate',
+		'resize',
+		'save',
+		'open',
+		'close'
+	],
+	colorScheme: {
+		main: '#414141', // make panels light-yellow
+		control: '#414141', // change controls color
+		controlContent: '#d9d9d9' // change controls color
+	}
+}).show();
 
 const UI = {
   render: function(content, where = "beforeend") {
-    const container = document.querySelector("#app");
-    container.insertAdjacentHTML(where, content);
+	 const container = document.querySelector("#app");
+	 container.insertAdjacentHTML(where, content);
   }
 };
 
