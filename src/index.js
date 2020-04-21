bc7a345d6a82ba9b22c7a117306b3adebf6b1732
Ashley Kolodziej <kolodz10@bu.edu>
2020-04-20 21:30:07 -0400

    Rough out kerning controls

--- src/index.js
+++ src/index.js
@@ -10,3 +10,44 @@ const UI = {
 };
 
 UI.render( "<h1>Learn Design</h1>", "afterbegin" );
+
+UI.render( `<div id="kerning" contenteditable="true">Wave</div>`,
+
+"afterbegin" );
+
+function getPosition() {
+    if (window.getSelection) {
+        var sel = window.getSelection();
+        if (sel.getRangeAt) {
+            return sel.getRangeAt(0).startOffset;
+        }
+    }
+    return null;
+}
+
+
+function kernControls(e) {
+	var pressed = e.keyCode,
+		 altPressed = e.altKey;
+
+	switch( pressed ) {
+		case 18:
+			console.log( "Valid Alt" );
+			break;
+		case 37: case 39:
+			console.log( "Valid Arrow" );
+
+			if ( altPressed ) {
+				e.preventDefault();
+				console.log( getPosition() );
+			};
+
+			break;
+		default:
+			e.preventDefault();
+	}
+}
+
+var kernArea = document.getElementById( `kerning` );
+
+kernArea.addEventListener( 'keydown', kernControls );
