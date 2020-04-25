f38293e54b6b987427428bd447eed8bd90d324c1
Ashley Kolodziej <kolodz10@bu.edu>
2020-04-25 18:16:02 -0400

    Documentation, id to class cleanup

--- src/components/kernable.js
+++ src/components/kernable.js
@@ -1,28 +1,44 @@
 import React from 'react';
 import { stringToSpans } from './library';
 
+/**
+* "Intent" only happens when you hit an arrow, so when you
+* first encounter a kernable word, you haven't shown intent yet.
+*/
+
 const initialState = {
 	intendedDirection: null,
 };
 
 class Kernable extends React.Component {
+	/**
+	* Build a kernable word from settings, and help our event
+	* listeners understand the context of "this".
+	*/
 	constructor (props) {
 		super(props);
 		this.state = {
 			...initialState,
 		};
 
-		// Pass "this" to callbacks
 		this.kern = this.kern.bind(this);
 		this.resetState = this.resetState.bind(this);
 	}
 
+	/**
+	* Resets the state to null.
+	*/
 	resetState() {
 		this.setState({
 			intendedDirection: null,
 		});
 	}
 
+	/**
+	* Get the letter or letters to kern, based on the user selection
+	* and with assumptions about intended direction based on previous
+	* arrow press. ☕
+	*/
 	getSelected( intendedDirection ) {
 		if ( window.getSelection ) {
 			var sel = window.getSelection();
@@ -41,6 +57,9 @@ class Kernable extends React.Component {
 		return null;
 	}
 
+	/**
+	* Get the existing kerning value, if it exists.
+	*/
 	getKerning( letter ) {
 		var kerning = 0;
 
@@ -51,6 +70,9 @@ class Kernable extends React.Component {
 		return kerning;
 	}
 
+	/**
+	* Adjust kerning value up or down based on pressed arrow direction.
+	*/
 	adjustAmount( amount, direction ) {
 		if ( direction === "left" ) {
 			amount--;
@@ -61,12 +83,19 @@ class Kernable extends React.Component {
 		return amount;
 	}
 
+	/**
+	* Kern a single letter based on user input.
+	*/
 	kernLetter( letter, direction ) {
 		var amount = this.getKerning( letter );
 		amount = this.adjustAmount( amount, direction );
 		letter.style.letterSpacing = `${amount}px`;
 	}
 
+	/**
+	* Kern the current selection based on user input.
+	* Supports both single and multiple letter selections.
+	*/
 	kernSelection( selection, direction ) {
 		if ( selection.length ) {
 			for ( var i = 0; i < selection.length; i++ ) {
@@ -77,6 +106,11 @@ class Kernable extends React.Component {
 		}
 	}
 
+	/**
+	* Kern, baby kern!
+	* Disallows all keypresses except alt, meta (windows/mac key), and arrows.
+	* Sets the intended direction or requests kerning based on keypress values.
+	*/
 	kern(e) {
 		var pressed = e.keyCode,
 			 altPressed = e.altKey,
@@ -91,7 +125,7 @@ class Kernable extends React.Component {
 			e.preventDefault();
 		}
 
-		// Prevent default for all keypresses except alt and arrows.
+		// Prevent default for all keypresses except alt, meta, and arrows.
 		switch( pressed ) {
 			case 18: case 224:
 				break;
@@ -125,9 +159,16 @@ class Kernable extends React.Component {
 		}
 	}
 
+	/**
+	* Go time!
+	*
+	* contentEditable is used to allow cursor and selection only.
+	* This content isn't actually editable because we disable all key
+	* default behaviors besides alt, meta, and arrows.
+	*/
 	render() {
 		return (
-			<div id="kerning"
+			<div className="kernable"
 				contentEditable="true"
 				onKeyDown={this.kern}
 				onClick={this.resetState}>
