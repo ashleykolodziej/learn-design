70c98cc01fa4302c05171fc1f37742358dc4575f
Ashley Kolodziej <kolodz10@bu.edu>
2020-04-25 18:34:03 -0400

    Resolve child key item warnings

--- src/components/library.js
+++ src/components/library.js
@@ -4,7 +4,7 @@ export const stringToSpans = ( str ) => {
 	let newHTML = [];
 
 	for ( var i = 0; i < str.length; i++ ) {
-		newHTML.push(<span>{str[i]}</span>);
+		newHTML.push(<span key={i}>{str[i]}</span>);
 	}
 
 	return newHTML;
