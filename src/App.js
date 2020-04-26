0d312dd6c16e73e3c1900cfd7fbfebaf9cf4e7b0
Ashley Kolodziej <kolodz10@bu.edu>
2020-04-25 21:09:22 -0400

    First crack at exercises

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Kernable from 'components/kernable/kernable';
+import Exercise from 'components/exercise/exercise';
 
 import "styles.scss";
 
 function App() {
   return (
     <div className="App">
+    	<Exercise />
       <Kernable word="Wave"/>
       <Kernable word="Ashley"/>
     </div>
---
+++ src/components/exercise/exercise.js
@@ -0,0 +1,51 @@
+import React, { Component, Fragment } from 'react';
+import Kernable from 'components/kernable/kernable';
+import data from "./data";
+import './exercise.scss';
+
+const component = data.component[0];
+
+/**
+* A set of explicitly supported components for exercises.
+*
+* Why?
+*
+* Because React needs the component name to be assigned to a capitalized
+* variable in order to know to render it as a component and not HTML.
+*
+* See https://reactjs.org/docs/jsx-in-depth.html#choosing-the-type-at-runtime
+*/
+
+const Supported = {
+	Kernable
+};
+
+class Exercise extends Component {
+	/**
+	* Returns React elements for each item in a
+	* definition tree.
+	*
+	* See https://reactjs.org/docs/jsx-in-depth.html#choosing-the-type-at-runtime
+	*/
+
+	createElement = ( component ) => {
+		return( React.createElement(
+			Supported[component.type],
+			component.props
+		) );
+	}
+
+	render() {
+		return (
+			<Fragment>
+				<section className="exercise">
+					<h3 className="exercise-title">{data.title}</h3>
+					<p className="exercise-directions">{data.directions}</p>
+					{this.createElement( component )}
+				</section>
+			</Fragment>
+		);
+	}
+}
+
+export default Exercise;
