9db7e23b7016973d7658ef4ff87a8a6915a3ba16
Ashley Kolodziej <kolodz10@bu.edu>
2020-05-08 20:10:02 -0400

    Add Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import Kernable from 'components/kernable/kernable';
 import Exercise from 'components/exercise/exercise';
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import { Button } from "@chakra-ui/core";
 
 import "styles.scss";
 
 function App() {
 	return (
-		<div className="App">
-			<Exercise />
-			<Exercise name="sample" />
-		</div>
+		<ThemeProvider>
+			<CSSReset />
+			<div className="App">
+				<Button variantColor="green">Button</Button>
+				<Exercise />
+				<Exercise name="sample" />
+			</div>
+		</ThemeProvider>
 	);
 }
 
