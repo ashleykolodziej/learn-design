ae33183198deb1430b249e6fadb6aff568394a00
Ashley Kolodziej <kolodz10@bu.edu>
2020-05-26 20:52:23 -0400

    Refactor profile page

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
 import { Button } from "@chakra-ui/core";
-import { AuthContext } from 'contexts/auth';
+import { AuthContext, authDefaults, UserContext, SiteContext } from 'contexts/auth';
 import { LoadButton } from 'components/WPSubmit';
 
 import wpcomFactory from 'wpcom';
@@ -8,62 +8,77 @@ import wpcomOAuthFactory from 'wpcom-oauth-cors';
 
 //setContext( mediaFiles );
 
-async function setAuths( context, setContext, setIsUploading ) {
+async function getAuth( context, setAuth, setIsUploading ) {
 	const clientID = context.client_id,
 		wpcomOAuth = wpcomOAuthFactory( clientID );
 
 	wpcomOAuth.get( ( auth ) => {
-		setContext( Object.assign( context, {
+		console.log(auth);
+		setAuth( Object.assign( authDefaults, {
 			'auth': auth,
 			'wpcom': wpcomFactory( auth.access_token )
-		} ) );
-	} );
-
-	const auth = context.auth,
-			wpcom = context.wpcom,
-			user = wpcom.me(),
-			site = wpcom.site( auth.site_id );
-
-	await user.settings().get( { meta: true } ).then( ( data ) => {
-		setContext( Object.assign( context, {
-			'user': data,
-			is_logged_in: true
-		} ) );
-		setIsUploading( false );
-	} ).catch( ( error ) => {
-		setContext( Object.assign( context, {
-			'user': null
-		} ) );
-		setIsUploading( false );
-	} );
-
-	await site.get().then( ( data ) => {
-		setContext( Object.assign( context, {
-			'site': data
-		} ) );
-	} ).catch( ( error ) => {
-		console.warn( error );
-		setContext( Object.assign( context, {
-			'site': null
 		} ) );
 	} );
 
 	return;
 }
 
+async function getUser( authContext, setUser, setIsUploading ) {
+	const wpcom = authContext.wpcom,
+			user = wpcom.me();
+
+	await user.settings().get().then( ( data ) => {
+		setUser( {
+			'user': data,
+			'is_logged_in': true,
+		} );
+		setIsUploading( false );
+	} ).catch( ( error ) => {
+		setUser( {
+			'user': null,
+			'is_logged_in': false,
+		} );
+		setIsUploading( false );
+	} );
+}
+
+async function getSite( authContext, setSite, setIsUploading ) {
+	console.log(authContext);
+	const wpcom = authContext.wpcom,
+			site = wpcom.site( authContext.auth.site_id );
+
+	await site.get().then( ( data ) => {
+		setSite( {
+			'site': data,
+			'site_is_loading': false,
+		} );
+	} ).catch( ( error ) => {
+		console.warn( error );
+		setSite( {
+			'site': null,
+			'site_is_loading': 'error',
+		} );
+	} );
+}
+
 function LoginButton( props ) {
-	const [ context, setContext ] = useContext( AuthContext );
+	const [ auth, setAuth ] = useContext( AuthContext );
+	const [ user, setUser ] = useContext( UserContext );
+	const [ site, setSite ] = useContext( SiteContext );
 	const [ isUploading, setIsUploading ] = useState( true );
 
 	useEffect( () => {
-		setAuths( context, setContext, setIsUploading );
-	}, [ context, setContext, setIsUploading ]);
+		getAuth( auth, setAuth, setIsUploading )
+		.then( () => {
+			getUser( auth, setUser, setIsUploading );
+			getSite( auth, setSite, setIsUploading );
+		} );
+	}, [ auth, setAuth, setIsUploading ]);
 
 	return (
 		<LoadButton bg="transparent" border="1px" mr={5} isLoading={ isUploading } width="auto">
-			{ console.log(context.user) }
-			{ context.is_logged_in ?
-				<span>Hi, { context.user.first_name }!</span>
+			{ auth.is_logged_in ?
+				<span>Hi, { user.first_name }!</span>
 			:
 				<span>Log in to WordPress</span>
 			}
